bee2c4a89e69f001d6ec40d0f1b58617d613b0eb
Ckyyy002 <162990675+Ckyyy002@users.noreply.github.com>
2025-05-10 23:53:46 +0700

    Update and rename 12_147.c to 12.c

--- Chapter_2/12.c
+++ Chapter_2/12.c
@@ -9,9 +9,9 @@ int main() {
     printf("Input the distance traveled by the jet (m): ");
     scanf("%lf", &s);
 
-    v = (v*1000)/3600;
-    t = 2*s/v;
-    a = v/t;
+    v = (v * 1000) / 3600;
+    t = 2 * s / v;
+    a = v / t;
 
     printf("The jet's acceleration was %.2lf m/s^2\n", a);
     printf("Time spent by the jet was %.2lf s\n", t);
