02bb3009c61ebb711ad298e629a404af4e82eb3c
Lucky Himawan Prasetya <162990675+Ckyyy002@users.noreply.github.com>
2025-05-11 18:43:25 +0700

    Update 8.c

--- Chapter_7/8.c
+++ Chapter_7/8.c
@@ -1,58 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    int votes[5][4] = {
-        {192, 48, 206, 37},
-        {147, 90, 312, 21},
-        {186, 12, 121, 38},
-        {114, 21, 408, 39},
-        {267, 13, 382, 29}
-    };
-    int totalVotes[4] = {0}, overallVotes = 0;
+int main(){
+    double thisYear[12], lastYear[12], totalThisYear = 0, totalLastYear = 0;
+    const char *months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
-    printf("Precinct\tA\tB\tC\tD\n");
-    for(int i = 0; i < 5; i++){
-        printf("%d\t\t", i + 1);
-        for(int j = 0; j < 4; j++){
-            printf("%d\t", votes[i][j]);
-            totalVotes[j] += votes[i][j];
-        }
-        printf("\n");
+    printf("Enter the rainfall data (this year, last year) for each month:\n");
+
+    for(int i = 0; i < 12; i++){
+        scanf("%lf %lf", &thisYear[i], &lastYear[i]);
+        totalThisYear += thisYear[i];
+        totalLastYear += lastYear[i];
     }
 
-    for(int i = 0; i < 4; i++){
-        overallVotes += totalVotes[i];
-    }
-
-    printf("\nTotal votes for each candidate:\n");
-    for(int i = 0; i < 4; i++){
-        printf("Candidate %c: %d (%.2f%%)\n", 'A' + i, totalVotes[i], (totalVotes[i] * 100.0) / overallVotes);
+    printf("    Table of monthly rainfall\n");
+    printf("          ");
+    for(int i = 0; i < 12; i++) {
+        printf("%-9s", months[i]);
     }
     
-    int winnerIndex = -1;
-    for(int i = 0; i < 4; i++){
-        if((totalVotes[i] * 100.0) / overallVotes > 50.0){
-            winnerIndex = i;
-            break;
-        }
+    printf("\nThis year ");
+    for(int i = 0; i < 12; i++){
+        printf("%-9.1lf", thisYear[i]);
     }
 
-    if(winnerIndex != -1){
-        printf("\nCandidate %c wins with more than 50%% of the total votes!\n", 'A' + winnerIndex);
+    printf("\nLast year ");
+    for (int i = 0; i < 12; i++) {
+        printf("%-9.1lf", lastYear[i]);
     }
-    else{
-        int max1 = -1, max2 = -1;
-        for(int i = 0; i < 4; i++){
-            if (max1 == -1 || totalVotes[i] > totalVotes[max1]){
-                max2 = max1;
-                max1 = i;
-            } 
-            else if(max2 == -1 || totalVotes[i] > totalVotes[max2]){
-                max2 = i;
-            }
-        }
-        printf("\nNo candidate received more than 50%% of the votes.\nA runoff is required between Candidate %c and Candidate %c.\n", 'A' + max1, 'A' + max2);
-    }
+
+    printf("\nTotal rainfall this year: %.1lf\n", totalThisYear);
+    printf("Total rainfall last year: %.1lf\n", totalLastYear);
+    printf("Average monthly rainfall for this year: %.1lf\n", totalThisYear / 12);
+    printf("Average monthly rainfall for last year: %.1lf\n", totalLastYear / 12);
 
     return 0;
 }
